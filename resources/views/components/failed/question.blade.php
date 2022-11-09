e1ef58160c6cfa99425fa60f69ffbc5f274c1bc8
WahyuSapurata <wm337708@gmail.com>
2022-11-10 05:35:27 +0800

    add error question

--- resources/views/components/failed/question.blade.php
+++ resources/views/components/failed/question.blade.php
@@ -34,4 +34,25 @@
             target.previousElementSibling.focus()
         }
     }
+
+    @error('errors')
+        @if (session()->get('chance') == 0)
+            setTimeout(() => {
+                location.replace('/quiz/question/{{ old('question') + 1 }}')
+            }, 2000);
+        @endif
+        Swal.fire({
+            title: 'Opps!',
+            @if (session()->get('chance') == 0)
+                text: 'Jawaban kamu masih salah, kesempatan kamu sudah habis',
+            @else
+                text: 'Jawaban kamu masih salah, kesempatan kamu tinggal {{ session()->get('chance') }} lagi',
+            @endif
+            imageUrl: '/img/salah.png',
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+            showConfirmButton: false,
+            timer: 4000,
+        })
+    @enderror
 </script>
